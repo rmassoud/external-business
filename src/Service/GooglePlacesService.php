0cd82443c6d267e3efc7273fadfdbfa437135b97
codestream <rmasoad@gmail.com>
2017-10-19 12:34:42 +1100

    Add some fixes

---
+++ src/Service/GooglePlacesService.php
@@ -0,0 +1,162 @@
+<?php namespace Codestream\ExternalBusiness\Service;
+
+use Codestream\ExternalBusiness\Model\ExternalBusiness;
+use Codestream\ExternalBusiness\Model\ExternalBusinessReview;
+use SKAgarwal\GoogleApi\PlacesApi;
+
+class GooglePlacesService implements ProviderServiceInterface
+{
+
+    /**
+     * @var array
+     */
+    private $dayMap = [
+        1 => 'Monday',
+        2 => 'Tuesday',
+        3 => 'Wednesday',
+        4 => 'Thursday',
+        5 => 'Friday',
+        6 => 'Saturday',
+        0 => 'Sunday',
+    ];
+
+
+    private $google_places;
+
+    private $key = 'AIzaSyAocvDCgiUaD9078kspYreZf0aMQSY9A3E';
+
+    public function __construct()
+    {
+        if($this->google_places == null) {
+            $this->google_places = new PlacesApi($this->key);
+        }
+    }
+
+    public function getProviderName()
+    {
+        return "google";
+    }
+
+    public function findMatch($query, $longitude, $latitude)
+    {
+        $results = $this->google_places->placeAutocomplete($query, [
+            'location' => "$longitude,$latitude",
+            'radius' => 500,
+            'strictbounds' => ''
+        ]);
+        if($results['status'] == 'OK') {
+            return $results['predictions'][0]['place_id'];
+        } else {
+            return null;
+        }
+    }
+
+    public function getBusiness($uuid, $params = [])
+    {
+        try {
+            $result = $this->google_places->placeDetails($uuid);
+            if($result) {
+                $business = new ExternalBusiness();
+                $business->setId($uuid);
+                $business->setProviderName($this->getProviderName());
+                $business->setData($result);
+                $business->setName($result['result']['name']);
+                $business->setOpeningHours( $this->getOpeningHours($result));
+                $business->setOpenNow($result['result']['opening_hours']['open_now']);
+                $business->setReviews( $this->getReviews($result));
+                $business->setState($this->getAddressComponent($result, 'administrative_area_level_1'));
+                $business->setCity($this->getAddressComponent($result, 'locality'));
+                $business->setFormattedAddress( $this->getFormattedAddress($result) );
+                $business->setCategories($this->getTags($result));
+                $business->setRating($result['result']['rating']);
+                $business->setPhone($result['result']['international_phone_number']);
+                $business->setDisplayPhone($result['result']['formatted_phone_number']);
+                $business->setLatitude($result['result']['geometry']['location']['lat']);
+                $business->setLongitude($result['result']['geometry']['location']['lng']);
+                $business->setWebsite($result['result']['website']);
+                $business->setPrice(3); //todo
+                $business->setPhotos($this->getPhotos($result));
+
+                return $business;
+            }
+        } catch (ContextErrorException $e) {
+            return null;
+        }
+    }
+
+    private function getOpeningHours($result) {
+        if($result['result']['opening_hours']) {
+            foreach($this->dayMap as $dayIndex => $day) {
+                $openingHours[$dayIndex] = [
+                    'day_name' => $day,
+                ];
+                foreach( $periods = $result['result']['opening_hours']['periods'] as $key => $period ) {
+                    if($period['close']['day'] == $dayIndex) {
+                        $openingHours[$dayIndex]['periods'][] = [
+                            'open_time' => date_format(date_create_from_format ("Hi", $period['open']['time']), 'h:ia'),
+                            'close_time' => date_format(date_create_from_format ("Hi", $period['close']['time']), 'h:ia'),
+                        ];
+                    }
+                }
+            }
+            return $openingHours;
+        }
+        return null;
+    }
+
+    private function getReviews($business) {
+        $reviews = [];
+        foreach($business['result']['reviews'] as $review_data) {
+            $review = new ExternalBusinessReview();
+            $review->setAuthorName($review_data['author_name']);
+            $review->setAuthorUrl($review_data['author_url']);
+            $review->setProfilePhotoUrl($review_data['profile_photo_url']);
+            $review->setRating($review_data['rating']);
+            $review->setText($review_data['text']);
+            $review->setTime($review_data['time']);
+            $review->setRelativeTimeDescription($review_data['relative_time_description']);
+            $reviews[] = $review;
+        }
+        return $reviews;
+    }
+
+    private function getAddressComponent($result, $component)
+    {
+        if(isset($result['result']['address_components'])) {
+            foreach( $result['result']['address_components'] as $address_component) {
+                if(in_array($component, $address_component['types'])) {
+                    return $address_component['short_name'];
+                }
+            }
+        }
+        return null;
+    }
+
+    private function getFormattedAddress($result) {
+        if(isset($result['result']['formatted_address'])) {
+            return explode(', ', $result['result']['formatted_address']);
+        }
+        return null;
+    }
+
+    private function getTags($result) {
+        $tags = [];
+        foreach($result['result']['types'] as $type) {
+            $tags[] = [
+                'alias' => $type,
+                'title' => ucwords( str_replace("_", " ", $type) ),
+            ];
+        }
+        return $tags;
+    }
+
+    private function getPhotos($result) {
+        $photos = [];
+        foreach($result['result']['photos'] as $photo) {
+            $photo_reference = $photo['photo_reference'];
+            $photos[] = "//maps.googleapis.com/maps/api/place/photo?photoreference=$photo_reference&key=$this->key";
+        }
+        return $photos;
+    }
+
+}
---
+++ src/Service/YelpService.php
@@ -0,0 +1,189 @@
+<?php namespace Codestream\ExternalBusiness\Service;
+
+use Codestream\ExternalBusiness\Model\ExternalBusiness;
+use Codestream\ExternalBusiness\Model\ExternalBusinessReview;
+use Stevenmaguire\OAuth2\Client\Provider\Yelp;
+use Stevenmaguire\Yelp\Exception\HttpException;
+use Stevenmaguire\Yelp\v3\Client;
+
+
+class YelpService
+{
+
+    private $client;
+
+    /**
+     * @var array
+     */
+    private $dayMap = [
+        1 => 'Monday',
+        2 => 'Tuesday',
+        3 => 'Wednesday',
+        4 => 'Thursday',
+        5 => 'Friday',
+        6 => 'Saturday',
+        0 => 'Sunday',
+    ];
+
+    public function __construct()
+    {
+        if($this->client == null) {
+            $provider = new Yelp([
+                'clientId'          => 'EDzcj0m7qWXlWWG0igfPjQ',
+                'clientSecret'      => '9cAPlhbunfcsrgcR52Y5ARbAl7Qq3COrrGbvAxEQRYdveg6nKttOukNItTNc8WX6'
+            ]);
+            $this->client = new Client(array(
+                'accessToken' => (string) $provider->getAccessToken('client_credentials'),
+                'apiHost' => 'api.yelp.com'
+            ));
+        }
+    }
+
+    public function getProviderName()
+    {
+        return "yelp";
+    }
+
+    /**
+     * @param $query
+     * @param $longitude
+     * @param $latitude
+     * @return null
+     */
+    public function findMatch($query, $longitude, $latitude)
+    {
+        $results = $this->client->getAutocompleteResults( [
+            'text' => $this->removeStopdWords($query),
+            'latitude' => $longitude,
+            'longitude' => $latitude,
+            'locale' => 'en_AU',
+        ]);
+
+        if( !empty($results->businesses) ) {
+            return $results->businesses[0]->id;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param $uuid
+     * @param array $params
+     * @return null|\Stevenmaguire\Yelp\v3\stdClass
+     */
+    public function getBusiness($uuid, $params = [])
+    {
+        try {
+            $result = $this->client->getBusiness($uuid, $params);
+            $business = new ExternalBusiness();
+            $business->setId($uuid);
+            $business->setProviderName($this->getProviderName());
+            $business->setData($result);
+            $business->setName($result->name);
+            $business->setOpeningHours( $this->getOpeningHours($result));
+            $business->setOpenNow( isset($result->hours) ? $result->hours[0]->is_open_now : null);
+            $business->setReviews($this->getReviews($uuid));
+            $business->setRating($this->getRating($business->getReviews()));
+            $business->setState($result->location->state);
+            $business->setCity($result->location->city);
+            $business->setFormattedAddress($result->location->display_address);
+            $business->setCategories($result->categories);
+            $business->setPhone($result->phone);
+            $business->setDisplayPhone($result->display_phone);
+            $business->setLatitude($result->coordinates->latitude);
+            $business->setLongitude($result->coordinates->longitude);
+            $business->setPrice(isset($result->price) ? strlen($result->price) * 5 / 4 : null);
+            $business->setPhotos($this->getPhotos($result));
+//            $business->setOpenNow(!$result->is_closed);
+
+            return $business;
+
+        } catch(HttpException $e) {
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * @param $uuid
+     * @param array $params
+     * @return null|\Stevenmaguire\Yelp\v3\stdClass
+     */
+    public function getReviews($uuid, $params = []) {
+        try
+        {
+            $result = $this->client->getBusinessReviews($uuid, $params);
+
+            $reviews = [];
+            foreach($result->reviews as $review_data) {
+                $review = new ExternalBusinessReview();
+                $review->setAuthorName($review_data->user->name);
+                $review->setProfilePhotoUrl($review_data->user->image_url);
+                $review->setRating($review_data->rating);
+                $review->setText($review_data->text);
+                $review->setTime( strtotime($review_data->time_created));
+                $reviews[] = $review;
+            }
+
+            return $reviews;
+        }
+        catch (HttpException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    protected function removeStopdWords($query)
+    {
+        $stop_words = array("a", "an", "the");
+        return trim(preg_replace('/\b('.implode('|', $stop_words ).')\b/i','', $query));
+    }
+
+    private function getOpeningHours($business)
+    {
+        if(isset($business->hours)) {
+            $openingHours = [];
+            foreach($this->dayMap as $dayIndex => $day) {
+                $openingHours[$dayIndex] = [
+                    'day_name' => $day,
+                    'periods' => [],
+                ];
+                foreach( $periods = $business->hours[0]->open as $key => $period ) {
+                    if($period->day == $dayIndex) {
+                        $openingHours[$dayIndex]['periods'][] = [
+                            'open_time' => date_format(date_create_from_format ("Hi", $period->start), 'h:ia'),
+                            'close_time' => date_format(date_create_from_format ("Hi", $period->end), 'h:ia'),
+                        ];
+                    }
+                }
+            }
+            return $openingHours;
+        }
+        return null;
+    }
+
+    private function getRating($reviews)
+    {
+        $score = 0;
+        if($reviews !== null) {
+            foreach($reviews as $review) {
+                $score += $review->getRating();
+            }
+            return $score/sizeof($reviews);
+        }
+        return null;
+    }
+
+    private function getPhotos($result)
+    {
+        $photos = [];
+        foreach($result->photos as $photo){
+            $photos[] = preg_replace('#^https?://#', '//', $photo);
+        }
+        return $photos;
+    }
+
+}
